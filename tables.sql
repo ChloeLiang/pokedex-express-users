994cb816d4b6beffb1c720c2ed3d9b111a906791
ChloeLiang <nliangxin@gmail.com>
2018-10-03 22:02:56 +0800

    Add types

--- tables.sql
+++ tables.sql
@@ -16,3 +16,14 @@ CREATE TABLE IF NOT EXISTS users_pokemons (
   user_id INTEGER,
   pokemon_id INTEGER
 );
+
+CREATE TABLE IF NOT EXISTS types (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS pokemons_types (
+  id SERIAL PRIMARY KEY,
+  pokemon_id INTEGER,
+  type_id INTEGER
+);
